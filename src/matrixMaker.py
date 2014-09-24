d2309b8e49a13615167e852d298904c0e734e900
Albert Acuna <aacuna3@asu.edu>
2014-09-23 18:46:58 -0700

    updated file 9/23/2014 6:47PM

--- src/matrixMaker.py
+++ src/matrixMaker.py
@@ -1,9 +1,22 @@
  #!/usr/bin/python
 
 import random
- 
-size = 10
-file = open('matrix1_10.txt','w')
+import sys
+
+size = int(sys.argv[1])
+
+fileName= 'input/matrix1_' + str(size) + '.txt'
+
+file = open(fileName,'w')
+for i in range(size):
+    for j in range(size):
+	num = random.randint(1,10);
+	file.write('%d ' % num);
+    file.write('\n');
+file.close()
+
+fileName= 'matrix2_' + str(size) + '.txt'
+file = open(fileName,'w')
 for i in range(size):
     for j in range(size):
 	num = random.randint(1,10);
